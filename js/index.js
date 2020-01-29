8b94cc61a6f09bcc5d710d535e46438c15e37e95
Siddharth <siddharth.pand8y@gmail.com>
2020-01-29 23:55:13 +0530

    CSS: Iteration #3 - Background Box

--- js/index.js
+++ js/index.js
@@ -10,3 +10,13 @@ function tabSelect(calculatorSelected) {
         document.getElementById("selectedTab").style.transform="translate(100%, 0)"
     }
 }
+var total,operand=[], operation=[];
+function screenDisplayOptions(operand, value) {
+    if(operand === false) {
+        this.operation.push(value);
+        console.log('Operator', this.operation);
+    } else {
+        this.operand.push(value);
+        console.log('Operator', this.operand);
+    }
+}
