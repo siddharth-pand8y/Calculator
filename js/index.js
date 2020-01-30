45db49cbebcb9b4655a9ac9aaa6048da630f5ced
Siddharth <siddharth.pand8y@gmail.com>
2020-01-30 23:41:29 +0530

    JS: Iteration #1

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
 function tabSelect(calculatorSelected) {
-    if(calculatorSelected === true ) {
-        document.getElementById("calculatorButton").className="selectedAction";
-        document.getElementById("converterButton").removeAttribute("class");
-        document.getElementById("selectedTab").style.transform="translate(0, 0)"
-
-    } else {
-        document.getElementById("converterButton").className="selectedAction";
-        document.getElementById("calculatorButton").removeAttribute("class");
-        document.getElementById("selectedTab").style.transform="translate(100%, 0)"
-    }
+  if (calculatorSelected === true) {
+    document.getElementById('calculatorButton').className = 'selectedAction';
+    document.getElementById('converterButton').removeAttribute('class');
+    document.getElementById('selectedTab').style.transform = 'translate(0, 0)';
+  } else {
+    document.getElementById('converterButton').className = 'selectedAction';
+    document.getElementById('calculatorButton').removeAttribute('class');
+    document.getElementById('selectedTab').style.transform =
+      'translate(100%, 0)';
+  }
 }
-var total,operand=[], operation=[];
+var total,
+  operand = [],
+  operation = [],
+  numberInput = '';
+previousInputOperator = false;
+
+
 function screenDisplayOptions(operand, value) {
-    if(operand === false) {
-        this.operation.push(value);
-        console.log('Operator', this.operation);
-    } else {
-        this.operand.push(value);
-        console.log('Operator', this.operand);
+  document.getElementById('screenDisplayInput').value += value;
+  if (operand === false) {
+    this.operation.push(value);
+    console.log('Operator', this.operation);
+    this.operation.push(parseInt(this.numberInput));
+    this.numberInput = '';
+    this.previousInputOperator = true;
+  } else {
+    if (this.previousInputOperator !== true) {
+      console.log('Operand', this.operand);
     }
+    this.previousInputOperator = true;
+  }
+}
+
+function reset() {
+    document.getElementById('screenDisplayInput').value = '';
+}
+
+function Delete() {
+    console.log(document.getElementById('screenDisplayInput').value.length);
+    let a = document.getElementById('screenDisplayInput').value;
+    document.getElementById('screenDisplayInput').value= a.substring(0, a.length-1);
 }
