dd847ccfff4fab403c5a05ea06482af44941605b
Siddharth <siddharth.pand8y@gmail.com>
2020-01-31 23:57:10 +0530

    JS: Iteration #2

--- js/index.js
+++ js/index.js
@@ -13,32 +13,62 @@ function tabSelect(calculatorSelected) {
 var total,
   operand = [],
   operation = [],
-  numberInput = '';
+  numberInput = '',
+  output = 0;
 previousInputOperator = false;
-
 
 function screenDisplayOptions(operand, value) {
   document.getElementById('screenDisplayInput').value += value;
   if (operand === false) {
     this.operation.push(value);
     console.log('Operator', this.operation);
-    this.operation.push(parseInt(this.numberInput));
+    this.operand.push(parseInt(this.numberInput));
     this.numberInput = '';
     this.previousInputOperator = true;
   } else {
     if (this.previousInputOperator !== true) {
-      console.log('Operand', this.operand);
+      this.numberInput += value;
     }
-    this.previousInputOperator = true;
+    this.previousInputOperator = false;
   }
 }
 
 function reset() {
-    document.getElementById('screenDisplayInput').value = '';
+  document.getElementById('screenDisplayInput').value = '';
 }
 
 function Delete() {
-    console.log(document.getElementById('screenDisplayInput').value.length);
-    let a = document.getElementById('screenDisplayInput').value;
-    document.getElementById('screenDisplayInput').value= a.substring(0, a.length-1);
+  document.getElementById('screenDisplayInput').value = document
+    .getElementById('screenDisplayInput')
+    .value.substring(
+      0,
+      document.getElementById('screenDisplayInput').value.length - 1
+    );
+}
+
+function calculate() {
+  console.log(this.operation, this.operand);
+  if (this.operation.length + 1 === this.operand.length) {
+    this.output = 0;
+    for (let i = this.operand.length; i >= 0; i++) {
+      switch (this.operation[i]) {
+        case '+':
+          this.output += (this.operand[i + 1] + this.operand[i]);
+          break;
+        case '-':
+          this.output = (this.operand[i + 1] - this.operand[i]);
+          break;
+        case '*':
+          this.output = (this.operand[i + 1] * this.operand[i]);
+          break;
+        case '/':
+          this.output = (this.operand[i + 1] / this.operand[i]);
+          break;
+        default:
+          break;
+      }
+    }
+    console.log('Output', this.output);
+    document.getElementById('output').value = this.output;
+  }
 }
