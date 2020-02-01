4b85acca76d87276034805515d620f993cea7892
Siddharth <siddharth.pand8y@gmail.com>
2020-02-01 23:39:01 +0530

    JS: Iteration #3 - Taking a single operator input on an operand

--- js/index.js
+++ js/index.js
@@ -15,26 +15,41 @@ var total,
   operation = [],
   numberInput = '',
   output = 0;
-previousInputOperator = false;
+previousInputisOperator = false;
 
-function screenDisplayOptions(operand, value) {
-  document.getElementById('screenDisplayInput').value += value;
-  if (operand === false) {
-    this.operation.push(value);
-    console.log('Operator', this.operation);
-    this.operand.push(parseInt(this.numberInput));
-    this.numberInput = '';
-    this.previousInputOperator = true;
+var calculationInput = '',
+  calculationOutput = '';
+
+function handleCalculatorButtonPress(isOperand, value) {
+  this.calculationInput = document.getElementById('screenDisplayInput').value;
+  if (isOperand === true) {
+    this.previousInputisOperator = false;
+    this.calculationInput += value;
+    console.log('Operand', this.calculationInput);
+    document.getElementById('screenDisplayInput').value = this.calculationInput;
   } else {
-    if (this.previousInputOperator !== true) {
-      this.numberInput += value;
+    if(this.calculationInput.length === 0) {
+      console.warn('Operator entered before operand');
+    } else {
+      if(this.previousInputisOperator === true) {
+        this.calculationInput = this.calculationInput.slice(0, -1) + value;
+        console.log('Opertor #1',this.calculationInput);
+      } else {
+        this.calculationInput += value;
+        console.log('Opertor #2',this.calculationInput);
+      }
+      this.previousInputisOperator = true;
+      document.getElementById('screenDisplayInput').value = this.calculationInput;
     }
-    this.previousInputOperator = false;
   }
 }
 
 function reset() {
   document.getElementById('screenDisplayInput').value = '';
+  this.numberInput = '';
+  this.operation.length = 0;
+  this.operand.length = 0;
+  this.output = 0;
 }
 
 function Delete() {
@@ -47,28 +62,30 @@ function Delete() {
 }
 
 function calculate() {
-  console.log(this.operation, this.operand);
+  this.operand.push(parseInt(this.numberInput));
+  this.numberInput = '';
+
   if (this.operation.length + 1 === this.operand.length) {
     this.output = 0;
-    for (let i = this.operand.length; i >= 0; i++) {
+    for (let i = this.operand.length; i >= 0; i--) {
       switch (this.operation[i]) {
         case '+':
-          this.output += (this.operand[i + 1] + this.operand[i]);
+          this.output += this.operand[i + 1] + this.operand[i];
           break;
         case '-':
-          this.output = (this.operand[i + 1] - this.operand[i]);
+          this.output = this.operand[i + 1] - this.operand[i];
           break;
         case '*':
-          this.output = (this.operand[i + 1] * this.operand[i]);
+          this.output = this.operand[i + 1] * this.operand[i];
           break;
         case '/':
-          this.output = (this.operand[i + 1] / this.operand[i]);
+          this.output = this.operand[i + 1] / this.operand[i];
           break;
         default:
           break;
       }
     }
     console.log('Output', this.output);
-    document.getElementById('output').value = this.output;
+    document.getElementById('output').innerHTML = this.output;
   }
 }
